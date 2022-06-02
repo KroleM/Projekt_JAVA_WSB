f67b558b41724f40a90fed787750bef3a6232d76
Karol Marciniak <marciniakarol@gmail.com>
2022-06-02 15:13:33 +0200

    Okno Fiszki

---
+++ src/listeners/JezykCheckListener.java
@@ -0,0 +1,91 @@
+package listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+
+import program.OknoFiszki;
+
+/**
+ * Akcja wywo³ana CheckBoxami polski/angielski w karcie Fiszki
+ * Pozwala na rozpoczêcie wpisywania t³umaczeñ w wybranym jezyku
+ */
+public class JezykCheckListener implements ActionListener
+{
+	private JCheckBox checkbox1;
+	private JCheckBox checkbox2;
+	private JTextField pole1;
+	private JTextField pole2;
+	
+	public JCheckBox getCheckbox1() {
+		return checkbox1;
+	}
+	public void setCheckbox1(JCheckBox checkbox1) {
+		this.checkbox1 = checkbox1;
+	}
+	public JCheckBox getCheckbox2() {
+		return checkbox2;
+	}
+	public void setCheckbox2(JCheckBox checkbox2) {
+		this.checkbox2 = checkbox2;
+	}
+	public JTextField getPole1() {
+		return pole1;
+	}
+	public void setPole1(JTextField pole1) {
+		this.pole1 = pole1;
+	}
+	public JTextField getPole2() {
+		return pole2;
+	}
+	public void setPole2(JTextField pole2) {
+		this.pole2 = pole2;
+	}
+	
+	public JezykCheckListener(JCheckBox checkbox1, JCheckBox checkbox2, JTextField pole1, JTextField pole2)
+	{
+		super();
+		this.checkbox1 = checkbox1;
+		this.checkbox2 = checkbox2;
+		this.pole1 = pole1;
+		this.pole2 = pole2;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		try 
+		{
+			if(checkbox1.isSelected())
+			{
+				dezaktywacja(pole1, pole2);
+				checkbox2.setSelected(false);
+			}
+			else
+			{
+				aktywacja(pole1, pole2);
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}		
+	}
+	
+	private void dezaktywacja(JTextField pole1, JTextField pole2)
+	{
+		pole1.setEnabled(false);
+		//pole1.setEditable(false);
+		pole2.setEnabled(true);
+		//pole2.setEditable(true);
+	}
+	private void aktywacja(JTextField pole1, JTextField pole2)
+	{
+		pole1.setEnabled(true);
+		pole1.setEditable(true);
+		pole2.setEnabled(true);
+		pole2.setEditable(true);
+	}
+}
---
+++ src/program/OknoFiszki.java
@@ -0,0 +1,169 @@
+package program;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+import listeners.JezykCheckListener;
+import listeners.StartFiszkiListener;
+
+/**
+ * Okno do rozwi¹zywania i wprowadzania fiszek
+ * Jego otwarcie powoduje zablokowanie przycisków w oknie g³ównym
+ * Zamkniêcie okna fiszek powoduje odblokowanie przycisków w oknie g³ównym
+ * Zamkniêcie okna fiszek NIE powoduje zamkniêcia ca³ego programu
+ */
+public class OknoFiszki extends JFrame
+{
+	private JTextField fiszkaAngielska;
+	private JTextField fiszkaPolska;
+	private OknoGlowne okno;
+	private JCheckBox polski;
+	private JCheckBox angielski;
+	private JButton start;
+	private JButton wprowadzanie;
+	private static ArrayList<ParaSlow> paraSlow;
+	static {
+		paraSlow = new ArrayList<>(List.of(
+				new ParaSlow("rower", "bicycle"), 
+				new ParaSlow("siode³ko", "saddle"), 
+				new ParaSlow("widelec", "fork"),
+				new ParaSlow("opona", "tyre"),
+				new ParaSlow("zapiêcie", "lock"),
+				new ParaSlow("rama", "frame"),
+				new ParaSlow("piasta", "hub"),
+				new ParaSlow("hamulec", "brake"),
+				new ParaSlow("amortyzator", "shock absorber"),
+				new ParaSlow("kierownica", "handlebar"),
+				new ParaSlow("przerzutka", "gear"),
+				new ParaSlow("³ancuch", "chain"),
+				new ParaSlow("zêbatka", "sprocket"),
+				new ParaSlow("dêtka", "tube"),
+				new ParaSlow("szprycha", "spoke")
+				));
+	}
+
+	public JTextField getFiszkaAngielska() {
+		return fiszkaAngielska;
+	}
+	public void setFiszkaAngielska(JTextField fiszkaAngielska) {
+		this.fiszkaAngielska = fiszkaAngielska;
+	}
+	public JTextField getFiszkaPolska() {
+		return fiszkaPolska;
+	}
+	public void setFiszkaPolska(JTextField fiszkaPolska) {
+		this.fiszkaPolska = fiszkaPolska;
+	}
+	public OknoGlowne getOkno() {
+		return okno;
+	}
+	public void setOkno(OknoGlowne okno) {
+		this.okno = okno;
+	}
+	public JCheckBox getPolski() {
+		return polski;
+	}
+	public void setPolski(JCheckBox polski) {
+		this.polski = polski;
+	}
+	public JCheckBox getAngielski() {
+		return angielski;
+	}
+	public void setAngielski(JCheckBox angielski) {
+		this.angielski = angielski;
+	}
+	public JButton getStart() {
+		return start;
+	}
+	public void setStart(JButton start) {
+		this.start = start;
+	}
+	public JButton getWprowadzanie() {
+		return wprowadzanie;
+	}
+	public void setWprowadzanie(JButton wprowadzanie) {
+		this.wprowadzanie = wprowadzanie;
+	}
+	public static ArrayList<ParaSlow> getParaSlow() {
+		return paraSlow;
+	}
+	public static void setParaSlow(ArrayList<ParaSlow> paraSlow) {
+		OknoFiszki.paraSlow = paraSlow;
+	}
+	
+	public OknoFiszki(OknoGlowne okno)
+	{
+		super("Fiszki");
+		this.setOkno(okno);
+		setSize(500, 300);
+		JPanel panelGlowny = new JPanel(new GridLayout(5, 1, 10, 10));
+		panelGlowny.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		setContentPane(panelGlowny);
+		
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JPanel panel5 = new JPanel();
+		panel1.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel2.setLayout(new FlowLayout());
+		panel3.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel4.setLayout(new FlowLayout());
+		panel5.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		fiszkaPolska = new JTextField(15);
+		fiszkaAngielska = new JTextField(15);
+		polski = new JCheckBox("polski");
+		angielski = new JCheckBox("angielski");
+		start = new JButton("START");
+		wprowadzanie = new JButton("Wprowadzanie");
+		
+		JLabel fiszki = new JLabel("FISZKI");
+		fiszki.setFont(new Font("Calibri", Font.BOLD, 26));
+		
+		panel1.add(fiszki);
+		panel2.add(polski);
+		panel2.add(angielski);
+		panel3.add(start);
+		panel4.add(new JLabel("POLSKI"));
+		panel4.add(fiszkaPolska);
+		panel4.add(new JLabel("ANGIELSKI"));
+		panel4.add(fiszkaAngielska);
+		panel5.add(wprowadzanie);
+		
+		panelGlowny.add(panel1);
+		panelGlowny.add(panel2);
+		panelGlowny.add(panel3);
+		panelGlowny.add(panel4);
+		panelGlowny.add(panel5);
+		
+		polski.addActionListener(new JezykCheckListener(polski, angielski, fiszkaPolska, fiszkaAngielska));
+		angielski.addActionListener(new JezykCheckListener(angielski, polski, fiszkaAngielska, fiszkaPolska));
+		start.addActionListener(new StartFiszkiListener(this));
+		
+		//setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		addWindowListener (new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dispose();
+			}
+			public void windowClosed(WindowEvent e) {
+				getOkno().AktywujPrzyciski();
+			}
+		});
+		setVisible(true);
+	}
+		
+}
